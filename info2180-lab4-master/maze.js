9bef5ffff8420eea393ddda66d9e427fa0305d0c
gen90 <morg.hall@yahoo.com>
2018-10-19 09:33:03 -0500

    Exercise 1, Boundary 1 red

---
+++ info2180-lab4-master/maze.js
@@ -0,0 +1,13 @@
+//Exercise 1
+window.onload = function(){
+    let bound1 = document.getElementById('boundary1');
+    bound1.addEventListener("mouseover",change);
+   
+}
+function change(){
+    this.classList.add("youlose");
+    touch = true;
+}
+
+window.onload;
+
