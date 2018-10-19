c72a30ca37c1fb4e71d6f17d21350f92bd2eb914
gen90 <morg.hall@yahoo.com>
2018-10-19 09:34:29 -0500

    Exercise 2, All boundaries red

--- info2180-lab4-master/maze.js
+++ info2180-lab4-master/maze.js
@@ -2,11 +2,15 @@
 window.onload = function(){
     let bound1 = document.getElementById('boundary1');
     bound1.addEventListener("mouseover",change);
+
+    var allBound = document.querySelectorAll('.boundary');
+    for(var x = 1;x<allBound.length -1;x++){
+        allBound[x].addEventListener("mouseover",change);  
+    }
    
 }
 function change(){
     this.classList.add("youlose");
-    touch = true;
 }
 
 window.onload;
